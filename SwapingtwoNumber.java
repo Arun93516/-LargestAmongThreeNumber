32367ba4141a7f3c93460158b27885d2139fd276
Arun93516 <arunkumar93516@gmail.com>
2022-09-11 00:40:56 +0530

    Added: Welcome to the Java Program

---
+++ SwapingtwoNumber.java
@@ -0,0 +1,18 @@
+package elbaT;
+
+public class SwapingtwoNumber {
+
+	public static void main(String[] args) {
+
+		int a, b, c;
+		a = 10;
+		b = 12;
+		
+		c = a;
+		a = b;
+		b = c;
+		System.out.println("Swaping the Number Is A is:" + b);
+		System.out.println("Swaping the number is  B is :" + a);
+
+	}
+}
